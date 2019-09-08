beeaa1bf219283a445fb4a3b638bfd84a45d2f28
Pamela Abreu <43766342+pamelaabreu@users.noreply.github.com>
2019-09-08 17:06:58 -0400

    Fixed syntax errors for importing express app (#7)

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 // Express app
-const app = require("./app");
+const { app } = require("./app");
 
 // Default port
 const port = 3000;
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Shares Portfolio Backend server running on Port ${ process.env.PORT || port}.`);
 });
 
