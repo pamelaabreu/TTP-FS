e964cf383f17adab0d0ad063b13d8914f546da5c
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-08 19:03:19 -0400

    Created intial transaction POST endpoint

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -4,4 +4,17 @@ const express = require("express");
 // Initialize Transactions Express Router
 const transactionsRouter = express.Router();
 
+// POST - Create transaction
+transactionsRouter.post("/", (req, res) => {
+  const { email, ticket, shares_amount, transaction_price } = req.body;
+  const message = `Successfully recieved email: ${email}, ticket: ${ticket}, shares_amount: ${shares_amount}, transaction_price: ${transaction_price}.`;
+
+  res.status(200);
+  res.json({
+    message
+  });
+});
+
+// GET - Read all transactions
+
 module.exports = transactionsRouter;
