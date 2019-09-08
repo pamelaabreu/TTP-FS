14df953861ecc7047b0ac9af6b26e3dd9a4a33b2
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-08 19:56:32 -0400

    Modified the SQL return entry when creating a new user

--- backend/services/users.js
+++ backend/services/users.js
@@ -1,17 +1,17 @@
 // Database Connection
-const {db} = require('./dbConnect');
+const { db } = require("./dbConnect");
 
 // Inital user service object
 const userService = {};
 
 // Create user
 userService.create = (name, email, firebase_uid) => {
-    const cash_balance = 5000;
-    const sql = `
+  const cash_balance = 5000;
+  const sql = `
     INSERT INTO users (name, email, firebase_uid, cash_balance) VALUES
-    ($[name], $[email], $[firebase_uid], $[cash_balance]) RETURNING id;`;
+    ($[name], $[email], $[firebase_uid], $[cash_balance]) RETURNING id, cash_balance;`;
 
-    return db.one(sql, { name, email, firebase_uid, cash_balance });
+  return db.one(sql, { name, email, firebase_uid, cash_balance });
 };
 
 // Read all user information by email
