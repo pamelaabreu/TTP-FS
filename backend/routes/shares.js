79c5a639f969f857a7a80aeb8b5ae68126492692
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-08 19:26:37 -0400

    Created /shares/all GET endpoint to read all shares

--- backend/routes/shares.js
+++ backend/routes/shares.js
@@ -4,4 +4,15 @@ const express = require("express");
 // Initialize Shares Express Router
 const sharesRouter = express.Router();
 
+// GET - Read all shares
+sharesRouter.get("/all/:email", (req, res) => {
+    const { email } = req.params;
+    const message = `Successfully recieved email: ${email} for all shares.`;
+
+    res.status(200);
+    res.json({
+        message
+    });
+});
+
 module.exports = sharesRouter;
