d2a3164c966d2aa650674b15b48c014a9393ef50
Pamela Abreu <43766342+pamelaabreu@users.noreply.github.com>
2019-09-08 18:30:28 -0400

    Backend User Endpoints (#8)
    * Fixed syntax errors for importing express app
    
    * Inital user router file
    
    * Imported express dependency
    
    * Initialized user express router
    
    * Exported the user router
    
    * Imported the user router in app.js
    
    * Created the /user endpoint
    
    * Changed comments on app.js
    
    * Created initial user POST request endpoint
    
    * Created initial user info GET request endpoint
    
    * Created initial user cashBalance GET request endpoint
    
    * Updated user message request

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,8 @@ const cors = require("cors");
 // Intialize Express App
 const app = express();
 
-// Route File Imports
+// Routers File Imports
+const userRouter = require("./routes/users");
 
 // Middleware
 // Enable All CORS Requests
@@ -17,10 +18,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Parses application/json
 app.use(bodyParser.json());
 
-// Routes
-// Testing route
+// Endpoints
+// Testing endpoint
 app.use("/ping", (req, res) => {
   res.json({ pong: true });
 });
+
+// User endpoint
+app.use("/user", userRouter);
 
 module.exports = { app };
---
+++ backend/routes/users.js
@@ -0,0 +1,37 @@
+// Dependencies
+const express = require("express");
+
+// Initialize User Express Router
+const userRouter = express.Router();
+
+// POST - Create user
+userRouter.post("/", (req, res, next) => {
+  const { name, email, firebase_uid } = req.body;
+  const message = `Successfully recieved name:${name}, email:${email}, firebase_uid:${firebase_uid}.`;
+  res.status(200);
+  res.json({
+    message
+  });
+});
+
+// GET - Read all user information by email
+userRouter.get("/userInfo/:email", (req, res, next) => {
+  const { email } = req.params;
+  const message = `Successfully recieved email:${email} to get all user information.`;
+  res.status(200);
+  res.json({
+    message
+  });
+});
+
+// GET - Read user's cash balance information by email
+userRouter.get("/userCashBalance/:email", (req, res, next) => {
+  const { email } = req.params;
+  const message = `Successfully recieved email:${email} to get cash balance.`;
+  res.status(200);
+  res.json({
+    message
+  });
+});
+
+module.exports = userRouter;
