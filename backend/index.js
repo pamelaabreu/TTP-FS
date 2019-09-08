d0403723dd37ce0ba053468e510093bc78703e71
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-08 16:09:15 -0400

    Function to ensure app is running for debug purposes

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,11 @@
+// Express app
+const app = require("./app");
 
+// Default port
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Share Portfolio Backend running on Port ${port}.`);
+});
+
+module.exports = { app };
