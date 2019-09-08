e1712a26eec1f5aeba5bedc14324a4a9dab45bfa
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-08 14:30:56 -0400

    SQL command to create users table

--- backend/local_seed.sql
+++ backend/local_seed.sql
@@ -2,3 +2,12 @@ DROP DATABASE IF EXISTS stockportfolio;
 CREATE DATABASE stockportfolio;
 
 /c  stockportfolio;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    firebase_uid VARCHAR UNIQUE NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    name VARCHAR NOT NULL,
+    email VARCHAR UNIQUE NOT NULL,
+    cash_balance INT NOT NULL
+);
