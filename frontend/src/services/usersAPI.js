300606b48c631fc6ffb806aec41956f33272cdb2
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-09 19:17:14 -0400

    Created GET endpoint to read user's cash balance

--- frontend/src/services/usersAPI.js
+++ frontend/src/services/usersAPI.js
@@ -8,5 +8,7 @@ import baseUrl from "./backendUrlConnect";
 const usersAPIService = {};
 
 // Read user's cash balance
+usersAPIService.readAllUserCashBalance = email =>
+  axios.get(`${baseUrl}/user/userCashBalance/${email}`).then(res => res.data);
 
 export default usersAPIService;
