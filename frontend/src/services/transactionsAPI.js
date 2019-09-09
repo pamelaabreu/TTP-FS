617bad8da4a63d5c27cec117cb73e02c795ed3fc
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-09 02:59:16 -0400

    Exported the transactions service object

--- frontend/src/services/transactionsAPI.js
+++ frontend/src/services/transactionsAPI.js
@@ -3,3 +3,8 @@ import axios from "axios";
 
 // Base Url for Express API Endpoint
 import baseUrl from "./backendUrlConnect";
+
+// Inital transactions service object
+const transactionsAPIService = {};
+
+export default transactionsAPIService;
