de2c9fb1f38e3d884e454448ec26b665e8430528
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-09 01:06:09 -0400

    Created update shares in db functionality

--- backend/services/shares.js
+++ backend/services/shares.js
@@ -8,13 +8,25 @@ const shareService = {};
 shareService.create = (ticket, shares_amount, user_id) => {
   const sql = `
     INSERT INTO user_shares (ticket, shares_amount, user_id) VALUES
-    ($[ticket], $[shares_amount], $[user_id]) RETURNING id, ticket, shares_amount;
+    ($[ticket], $[shares_amount], $[user_id]) RETURNING ticket, shares_amount;
     `;
 
   return db.one(sql, { ticket, shares_amount, user_id });
 };
 
 // Update shares
+shareService.updateShare = (ticket, shares_amount, user_id) => {
+    const sql = `
+    UPDATE user_shares
+    SET
+        shares_amount = $[shares_amount]
+    WHERE
+        user_id=$[user_id] AND ticket=$[ticket]
+    RETURNING ticket, shares_amount;
+    `;
+
+    return db.one(sql, { ticket, shares_amount, user_id });
+  };
 
 // Read share
 shareService.readShare = (ticket, user_id) => {
