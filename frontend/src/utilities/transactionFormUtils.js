e1ae778183a87c9e446190cfe906929eff5afa02
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-09 19:30:38 -0400

    Initial transactionFormUtils file

---
+++ frontend/src/utilities/transactionFormUtils.js
@@ -0,0 +1,15 @@
+// Services
+import IEXAPIService from "../services/IEXAPI";
+
+// Inital shares service object
+const transactionFormUtils = {};
+
+// Check valid ticket symbol
+
+// Get current price of ticket
+
+// Check if quantity is whole number
+
+// Check if cashBalance enough
+
+export default transactionFormUtils;
