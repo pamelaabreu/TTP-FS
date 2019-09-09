21f4be9e00acc8df93ceea4d9ec2671805232d14
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-09 19:10:14 -0400

    Created initial usersAPI endpoint service file

---
+++ frontend/src/services/usersAPI.js
@@ -0,0 +1,12 @@
+// Dependencies
+import axios from "axios";
+
+// Base Url for Express API Endpoint
+import baseUrl from "./backendUrlConnect";
+
+// Inital transactions service object
+const usersAPIService = {};
+
+// Read user's cash balance
+
+export default usersAPIService;
