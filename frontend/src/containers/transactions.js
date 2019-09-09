546a9324825cec76affe700b45e8351feaac9621
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-09 02:49:31 -0400

    Created basic JSX for displaying the component

--- frontend/src/containers/transactions.js
+++ frontend/src/containers/transactions.js
@@ -2,7 +2,14 @@
 import React, { useState } from "react";
 
 const Transactions = props => {
-
+    return (
+        <div>
+            <h1>Transactions</h1>
+            <div>
+                <p>Display Transactions</p>
+            </div>
+        </div>
+    );
 };
 
 export default Transactions;
