caaf933da74aa05e674c3221a260c76f4309dd7a
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-09 03:47:19 -0400

    Created a useEffect to track and update transactionsList

--- frontend/src/containers/transactions.js
+++ frontend/src/containers/transactions.js
@@ -5,16 +5,28 @@ import React, { useState, useEffect } from "react";
 import transactionsAPIService from "../services/transactionsAPI";
 
 const Transactions = props => {
-    const [transactionsList, updateTransactionsList] = useState([]);
+  const [transactionsList, setTransactionsList] = useState([]);
 
-    return (
-        <div>
-            <h1>Transactions</h1>
-            <div>
-                <p>Display Transactions</p>
-            </div>
-        </div>
-    );
+  // Updates the transactionsList
+  useEffect(() => {
+    if (transactionsList.length === 0) {
+      transactionsAPIService
+        .readAllUserTransaction("default@testing.com")
+        .then(({ data }) => {
+          setTransactionsList(data);
+        })
+        .catch();
+    }
+  }, [transactionsList]);
+
+  return (
+    <div>
+      <h1>Transactions</h1>
+      <div>
+        <p>Display Transactions</p>
+      </div>
+    </div>
+  );
 };
 
 export default Transactions;
