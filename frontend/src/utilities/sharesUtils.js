af247b563709f6e93017ab1e02afe91540986cdd
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-09 06:26:46 -0400

    Created utility function to convert shares to reflect current price

--- frontend/src/utilities/sharesUtils.js
+++ frontend/src/utilities/sharesUtils.js
@@ -1,4 +1,24 @@
+// Services
+import IEXAPIService from "../services/IEXAPI";
+
 // Inital shares service object
 const sharesUtils = {};
+
+// Convert shares to reflect current price
+sharesUtils.convertSharesToCurrentPrice = ticket => {
+  let currentPrice = 0;
+
+  IEXAPIService.readStockInformation(ticket).then(
+    ({ lastSalePrice }) => (currentPrice = ticket * lastSalePrice)
+  );
+
+  return currentPrice;
+};
+
+// Rate current price of shares against opening price
+
+// Add total amount of all shares
+
+// Convert shares array to reflect current price and performance
 
 export default sharesUtils;
