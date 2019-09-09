d5a0e3f43300490df62e01b39781bc749d41fcf2
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-08 22:53:56 -0400

    Implemented the create transaction functionality for POST endpoint

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -10,15 +10,21 @@ const transactionsRouter = express.Router();
 // POST - Create transaction
 transactionsRouter.post("/", (req, res) => {
   const { email, ticket, shares_amount, transaction_price } = req.body;
-  const message = `Successfully recieved email: ${email}, ticket: ${ticket}, shares_amount: ${shares_amount}, transaction_price: ${transaction_price}.`;
 
-  res.status(200);
-  res.json({
-    message
-  });
+  transactionService
+    .create(email, ticket, shares_amount, transaction_price)
+    .then(data => {
+      const message = `Successfully recieved email: ${email}, ticket: ${ticket}, shares_amount: ${shares_amount}, transaction_price: ${transaction_price}.`;
+
+      res.status(200);
+      res.json({
+        data,
+        message
+      });
+    });
 });
 
-// GET - Read all transactions
+// GET - Read all of a user's transactions
 transactionsRouter.get("/allUserTransactions/:email", (req, res) => {
   const { email } = req.params;
   const message = `Successfully recieved email: ${email} for all transactions.`;
