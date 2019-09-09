7aba7a4e8681f89e78c322a7c61ea1b958b17459
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-09 04:50:56 -0400

    Replaced JSX with TransactionForm component

--- frontend/src/components/Portfolio.js
+++ frontend/src/components/Portfolio.js
@@ -3,41 +3,13 @@ import React from "react";
 
 // Component
 import Shares from "../containers/Shares";
+import TransactionForm from "../containers/TransactionForm";
 
 const Portfolio = props => {
   return (
     <div>
         <Shares />
-      <div>
-        <h3>Cash - $Users Balance</h3>
-        <form>
-          <label htmlFor="ticket">Ticket</label>
-          <input
-            type="text"
-            name="ticket"
-            placeholder="Ticket"
-            aria-label="Ticket"
-            aria-describedby="Ticket"
-            required
-          />
-
-          <label htmlFor="quantity">QTY</label>
-          <input
-            type="text"
-            name="quantity"
-            placeholder="Quantity"
-            aria-label="Quantity"
-            aria-describedby="Quantity"
-            required
-          />
-          <button
-            type="submit button"
-            //   disabled
-          >
-            Next
-          </button>
-        </form>
-      </div>
+        <TransactionForm />
     </div>
   );
 };
