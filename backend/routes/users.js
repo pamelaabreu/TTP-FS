b632144b528b3529de87927c8d72f150172dd168
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-08 20:16:16 -0400

    Used readUserCashBalance function to read user's cash balance information by email

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -13,13 +13,13 @@ userRouter.post("/", (req, res, next) => {
 
   userService
     .create(name, email, firebase_uid)
-    .then(({ id, cash_balance }) => {
-      const message = `Successfully recieved name:${name}, email:${email}, firebase_uid:${firebase_uid}. Created id: ${id} with cash balance of ${cash_balance}.`;
+    .then(data => {
+      const message = `Successfully recieved name:${name}, email:${email}, firebase_uid:${firebase_uid}. Created id: ${data.id} with cash balance of ${data.cash_balance}.`;
 
       res.status(200);
       res.json({
         message,
-        data: id
+        data
       });
     })
     .catch(err => {
@@ -50,11 +50,21 @@ userRouter.get("/userInfo/:email", (req, res, next) => {
 // GET - Read user's cash balance information by email
 userRouter.get("/userCashBalance/:email", (req, res, next) => {
   const { email } = req.params;
-  const message = `Successfully recieved email:${email} to get cash balance.`;
-  res.status(200);
-  res.json({
-    message
-  });
+
+  userService
+    .readUserCashBalance(email)
+    .then(data => {
+      const message = `Successfully recieved email:${email} to get cash balance.`;
+
+      res.status(200);
+      res.json({
+        data,
+        message
+      });
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = userRouter;
