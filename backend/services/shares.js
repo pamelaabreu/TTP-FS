a1bce73bbeb48b241ac55764b0fcee91cfe6ffc8
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-08 23:50:29 -0400

    Created create shares in db functionality

--- backend/services/shares.js
+++ backend/services/shares.js
@@ -4,4 +4,20 @@ const { db } = require("./dbConnect");
 // Inital shares service object
 const shareService = {};
 
+// Create shares
+shareService.create = (ticket, shares_amount, user_id) => {
+    const sql = `
+    INSERT INTO user_shares (ticket, shares_amount, user_id) VALUES
+    ($[ticket], $[shares_amount], $[user_id]) RETURNING id, ticket, shares_amount;
+    `;
+
+    return db.one(sql, {ticket, shares_amount, user_id});
+};
+
+// Update shares
+
+// Read share
+
+// Read all users's shares
+
 module.exports = shareService;
