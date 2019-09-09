6fab6851e7f6ea0d62dba9db8721a8410b59b7be
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-09 03:06:20 -0400

    Created API call function to hit /transactions/allUserTransactions endpoint

--- frontend/src/services/transactionsAPI.js
+++ frontend/src/services/transactionsAPI.js
@@ -7,4 +7,11 @@ import baseUrl from "./backendUrlConnect";
 // Inital transactions service object
 const transactionsAPIService = {};
 
+// Read all transactions
+transactionsAPIService.readAllUserTransaction = email => {
+  return axios
+    .get(`${baseUrl}/transactions/allUserTransactions/${email}`)
+    .then(res => res.data);
+};
+
 export default transactionsAPIService;
