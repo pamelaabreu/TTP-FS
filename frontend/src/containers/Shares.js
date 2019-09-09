09f76ec3244fc07ae0f88b8d9a0f17efce42c987
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-09 04:59:45 -0400

    Using array.map to iterate and render sharesList using the ListItems component

--- frontend/src/containers/Shares.js
+++ frontend/src/containers/Shares.js
@@ -1,14 +1,24 @@
 // Dependencies
 import React, { useState, useEffect } from "react";
 
+// Component
+import ListItems from "../components/ListItems";
+
 const Shares = props => {
   const [sharesList, setSharesList] = useState([]);
   const [portfolioAmount, setPortfolioAmount] = useState(0);
 
   return (
     <div>
-      <h1>Portfolio ($Cash Amount)</h1>
-      <p>Display shares here</p>
+      <h1>Portfolio (${portfolioAmount})</h1>
+      <div>
+          {
+             sharesList.map((value, index) => {
+                const transactionNote = `Ticket - shares_amount Shares $CurrentValue.00`;
+                return <ListItems key={index} note={transactionNote}/>
+             }) 
+          }
+      </div>
     </div>
   );
 };
