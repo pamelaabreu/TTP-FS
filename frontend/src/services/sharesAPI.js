5cd5113d38bd605040e23b4611e9f93381128ebe
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-09 05:15:59 -0400

    Created API request functionality to read all user's shares

--- frontend/src/services/sharesAPI.js
+++ frontend/src/services/sharesAPI.js
@@ -7,4 +7,8 @@ import baseUrl from "./backendUrlConnect";
 // Inital shares service object
 const sharesAPIService = {};
 
+// Read all of user's shares
+sharesAPIService.readAllShares = email =>
+  axios.get(`${baseUrl}/shares/all/${email}`).then(res => res.data);
+
 export default sharesAPIService;
