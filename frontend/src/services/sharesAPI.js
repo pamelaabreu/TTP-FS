55f32121c513d9ea57c99ac8094527fae2370447
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-09 05:15:24 -0400

    Initial sharesAPI service file

---
+++ frontend/src/services/sharesAPI.js
@@ -0,0 +1,10 @@
+// Dependencies
+import axios from "axios";
+
+// Base Url for Express API Endpoint
+import baseUrl from "./backendUrlConnect";
+
+// Inital shares service object
+const sharesAPIService = {};
+
+export default sharesAPIService;
