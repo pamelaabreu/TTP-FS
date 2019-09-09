b52d4bd4b05c4f685a7d80d797ae9fa98c5e1b1c
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-08 21:48:54 -0400

    Added comments on the transactions service file

--- backend/services/transactions.js
+++ backend/services/transactions.js
@@ -4,4 +4,8 @@ const { db } = require("./dbConnect");
 // Inital transaction service object
 const transactionService = {};
 
+// Create transaction
+
+// Read all transactions
+
 module.exports = transactionService;
