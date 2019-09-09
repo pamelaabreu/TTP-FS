243c0afc64ebf5b55e69a1f756fec6724a7441cb
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-09 04:44:02 -0400

    Initial Shares component

--- frontend/src/containers/Shares.js
+++ frontend/src/containers/Shares.js
@@ -1,2 +1,13 @@
 // Dependencies
 import React, { useState, useEffect } from "react";
+
+const Shares = props => {
+  return (
+    <div>
+      <h1>Portfolio ($Cash Amount)</h1>
+      <p>Display shares here</p>
+    </div>
+  );
+};
+
+export default Shares;
