3b9df50a4b997aa8af0a84d8ec1ccd24a546baad
Pamela Abreu <43766342+pamelaabreu@users.noreply.github.com>
2019-09-08 23:28:41 -0400

    Database Transaction services (#13)
    * Initial transactions service file
    
    * Imported the database connection functionality
    
    * Exported the transactionService object
    
    * Added comments on the transactions service file
    
    * Imported transaction database services
    
    * Changed the API endpoint for reading all transactions
    
    * Imported userService and created the create transaction service
    
    * Implemented the create transaction functionality for POST endpoint
    
    * Created the read all transactions from db functionality
    
    * Implemented the readAllUserTransaction function for GET endpoint

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,29 +1,50 @@
 // Dependencies
 const express = require("express");
 
+// Database Services
+const transactionService = require("../services/transactions");
+
 // Initialize Transactions Express Router
 const transactionsRouter = express.Router();
 
 // POST - Create transaction
-transactionsRouter.post("/", (req, res) => {
+transactionsRouter.post("/", (req, res, next) => {
   const { email, ticket, shares_amount, transaction_price } = req.body;
-  const message = `Successfully recieved email: ${email}, ticket: ${ticket}, shares_amount: ${shares_amount}, transaction_price: ${transaction_price}.`;
 
-  res.status(200);
-  res.json({
-    message
-  });
+  transactionService
+    .create(email, ticket, shares_amount, transaction_price)
+    .then(data => {
+      const message = `Successfully recieved email: ${email}, ticket: ${ticket}, shares_amount: ${shares_amount}, transaction_price: ${transaction_price}.`;
+
+      res.status(200);
+      res.json({
+        data,
+        message
+      });
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
-// GET - Read all transactions
-transactionsRouter.get("/:email", (req, res) => {
-    const { email } = req.params;
-    const message = `Successfully recieved email: ${email} for all transactions.`;
-  
-    res.status(200);
-    res.json({
-      message
+// GET - Read all of a user's transactions
+transactionsRouter.get("/allUserTransactions/:email", (req, res, next) => {
+  const { email } = req.params;
+
+  transactionService
+    .readAllUserTransaction(email)
+    .then(data => {
+      const message = `Successfully recieved email: ${email} for all transactions.`;
+
+      res.status(200);
+      res.json({
+        data,
+        message
+      });
+    })
+    .catch(err => {
+      next(err);
     });
-  });
+});
 
 module.exports = transactionsRouter;
---
+++ backend/services/transactions.js
@@ -0,0 +1,58 @@
+// Database Connection
+const { db } = require("./dbConnect");
+
+// Database Services
+const userService = require("./users");
+
+// Inital transaction service object
+const transactionService = {};
+
+// Create transaction
+transactionService.create = (
+  email,
+  ticket,
+  shares_amount,
+  transaction_price
+) => {
+  // First read the user email and return the user id associated with the email
+  return userService.readAllUserInfo(email).then(({ id }) => {
+    // Second create the transaction
+
+    const sql = `
+        INSERT INTO user_transactions (ticket, shares_amount, transaction_price, user_id) VALUES
+        ($[ticket], $[shares_amount], $[transaction_price], $[user_id]) RETURNING id;
+        `;
+
+    return db.one(sql, {
+      ticket,
+      shares_amount,
+      transaction_price,
+      user_id: id
+    });
+  });
+
+  // update share || create new share
+  // update user's cash balance
+};
+
+// Read all transactions
+transactionService.readAllUserTransaction = email => {
+  // First read the user email and return the user id associated with the email
+  return userService.readAllUserInfo(email).then(({ id }) => {
+    // Second read all of the user's transactions
+    const sql = `
+        SELECT
+            user_transactions.*
+        FROM
+            user_transactions
+        WHERE
+            user_id = $[id]
+        ORDER BY 
+            user_transactions.created_at DESC
+        `;
+
+    return db.any(sql, { id });
+  });
+};
+
+module.exports = transactionService;
