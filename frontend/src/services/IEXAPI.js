b0b5ea5fa9732c5fea04ad268c7b49a3fe28438b
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-09 05:21:23 -0400

    Initial IEXAPI API services file

---
+++ frontend/src/services/IEXAPI.js
@@ -0,0 +1,10 @@
+// Dependencies
+import axios from "axios";
+
+// Base Url for Express API Endpoint
+import baseUrl from "./backendUrlConnect";
+
+// Inital IEX API service object
+const IEXAPIService = {};
+
+export default IEXAPIService;
