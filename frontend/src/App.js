b0d363474133a5036bed599fd3ff277aae8c1a0b
Pamela Abreu <43766342+pamelaabreu@users.noreply.github.com>
2019-09-10 01:30:02 -0400

    Navbar Frontend Component (#21)
    * Initial navabar component
    
    * Imported navbar component to render
    
    * Imported react-router-dom dependency
    
    * Wrapped app component with HashRouter
    
    * Re-arranged JSX structure
    
    * Created the different route methods to access different pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,30 @@
 // Dependencies
-import React from 'react';
+import React from "react";
+import { Switch, Route, HashRouter } from "react-router-dom";
 
 // CSS
-import './App.css';
+import "./App.css";
 
 // Pages
 import Transactions from "./containers/Transactions";
 import Portfolio from "./components/Portfolio";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Transactions />
-        <Portfolio />
-      </header>
-    </div>
+    <HashRouter>
+      <div className="App">
+        <header className="App-header">
+          <Route path="/" component={Navbar} />
+        </header>
+        <div>
+          <Switch>
+            <Route path="/transactions" exact component={Transactions} />
+            <Route path="/portfolio" exact component={Portfolio} />
+          </Switch>
+        </div>
+      </div>
+    </HashRouter>
   );
 };
 
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,14 @@
+// Dependencies
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = props => {
+  return (
+    <div>
+      <Link to="/portfolio">Portfolio</Link>
+      <Link to="/transactions">Transactions</Link>
+    </div>
+  );
+};
+
+export default Navbar;
