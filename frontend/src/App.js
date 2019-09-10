643b12a21a4bbcb2638208ba9460f837ef8a11de
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-09 22:24:14 -0400

    Imported navbar component to render

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,11 +7,13 @@ import './App.css';
 // Pages
 import Transactions from "./containers/Transactions";
 import Portfolio from "./components/Portfolio";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar />
         <Transactions />
         <Portfolio />
       </header>
