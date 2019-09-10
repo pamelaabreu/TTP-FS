c441c703af0cd248d62d2eaba296cf8b4fe0d63b
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-10 12:36:17 -0400

    Modified bootstrap classes on showLoginOrSignup component

--- frontend/src/containers/ShowLoginOrSignup.js
+++ frontend/src/containers/ShowLoginOrSignup.js
@@ -11,24 +11,30 @@ const ShowLoginOrSignup = props => {
 
   return (
     <div className="container-fluid d-flex flex-column justify-content-center align-items-center">
-      <h1 className="display-4 mb-5 pb-5 text-white">Stock Portfolio App</h1>
-      <div className="container d-flex justify-content-center mb-3 pb-3">
-        <button
-          className={`btn m-3 ${showLoginOrSignup ? "btn-light" : "btn-dark"}`}
-          onClick={() => setLoginOrSignup(true)}
-        >
-          Login
-        </button>
-        <button
-          className={`btn m-3 ${showLoginOrSignup ? "btn-dark" : "btn-light"}`}
-          onClick={() => setLoginOrSignup(false)}
-        >
-          Signup
-        </button>
-      </div>
+      <h1 className="display-4 pb-5 text-white">Stock Portfolio App</h1>
+      <div>
+        <div className="container p-0 d-flex justify-content-start">
+          <button
+            className={`btn btn-lg btn-block m-0 mr-3 rounded-0 ${
+              showLoginOrSignup ? "btn-info" : "btn-outline-light"
+            }`}
+            onClick={() => setLoginOrSignup(true)}
+          >
+            Login
+          </button>
+          <button
+            className={`btn btn-lg btn-block m-0 rounded-0 ${
+              showLoginOrSignup ? "btn-outline-light" : "btn-info"
+            }`}
+            onClick={() => setLoginOrSignup(false)}
+          >
+            Signup
+          </button>
+        </div>
 
-      <div className="border border-primary">
-        {showLoginOrSignup ? <Login /> : <Signup />}
+        <div className="border border-warning mt-3">
+          {showLoginOrSignup ? <Login /> : <Signup />}
+        </div>
       </div>
     </div>
   );
