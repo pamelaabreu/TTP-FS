dc5e06aa5a4035ab02f25abcc07562081e218470
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-10 01:54:07 -0400

    Added comments

--- frontend/firebaseConfigExample.js
+++ frontend/firebaseConfigExample.js
@@ -1,3 +1,7 @@
+// Firebase Configuration Template
+// Replace key-pair values with you're own Firebase configurations
+// More information: https://support.google.com/firebase/answer/7015592?hl=en
+
 export default {
     apiKey: "",
     authDomain: "",
--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,7 +1,11 @@
+// Dependencies
 import app from "firebase/app";
 import "firebase/auth";
+
+// Firebase Configurations
 import config from "./firebaseConfig";
 
+// Initialize Firebase App
 app.initializeApp(config);
 
 export default app;
