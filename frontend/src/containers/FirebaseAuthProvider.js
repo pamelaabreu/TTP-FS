860e87cf955279f7b988fd0e187a1c180e471c6f
Pamela Abreu <pamelaabreu@pursuit.com>
2019-09-10 01:59:48 -0400

    Created componentDidMount lifecycle method to set user

--- frontend/src/containers/FirebaseAuthProvider.js
+++ frontend/src/containers/FirebaseAuthProvider.js
@@ -9,6 +9,16 @@ class FirebaseAuthProvider extends React.Component {
   state = {
     user: null
   };
+
+  componentDidMount() {
+    this.unsubscribe = firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ user });
+      } else {
+        this.setState({ user: null });
+      }
+    });
+  }
 }
 
 export default FirebaseAuthProvider;
